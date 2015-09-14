2322399fe025b5b51e0e0dda43592ddb76bcb740
Swagathor <nfleclerc@live.com>
2015-09-14 11:46:08 -0400

    trying this out
    Signed-off-by: Swagathor <nfleclerc@live.com>

---
+++ As1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+	
+	pid1 = fork();
+	pid2 = fork();
+	pid3 = fork();
+	pid4 = fork();
+	n = 10
+
+	if (pid1 < 0)
+		printf("Fork 1 isn't working!\n");
+	else {
+		printf("n (n-2))) binomial computations of integers n = 2, 3, 10			, start now!\n");
+	}
+
+
+}
