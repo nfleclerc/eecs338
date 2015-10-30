67abdfc0f40cc9e38b6489ba6d52c2d4d7ec3d0e
nfleclerc <nfleclerc@live.com>
2015-10-30 12:10:46 -0400

    set up writers
    Signed-off-by: nfleclerc <nfleclerc@live.com>

---
+++ as5/writer.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+void *writer(void *shared_data) {
+	struct shared_data_info *shared = (struct shared_data_info *)shared_data;
+
+	//wait(wrt)
+	if (sem_wait(shared->wrt) == -1){
+		perror("wait(wrt)");
+		pthread_exit(NULL);
+	}
+
+	//in critical section
+
+	printf("Writing the next Shakespearean play");
+
+	//crit section end
+
+	//signal(wrt)
+	if (sem_post(shared->wrt) == -1){
+		perror("signal(wrt)");
+
+		pthread_exit(NULL);
+	}
+
+	pthread_exit(NULL);
+
+}
